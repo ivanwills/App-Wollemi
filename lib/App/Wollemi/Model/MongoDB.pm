d9f83db85aef2c0390fd307f2fc47fc9de7bc808
Ivan Wills <ivan.wills@gmail.com>
2013-11-21 20:23:24 +1100

    Added backend mongodb

---
+++ lib/App/Wollemi/Model/MongoDB.pm
@@ -0,0 +1,28 @@
+package App::Wollemi::Model::MongoDB;
+use Moose;
+use namespace::autoclean;
+
+extends 'Catalyst::Model';
+
+=head1 NAME
+
+App::Wollemi::Model::MongoDB - Catalyst Model
+
+=head1 DESCRIPTION
+
+Catalyst Model.
+
+=head1 AUTHOR
+
+ivan,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+__PACKAGE__->meta->make_immutable;
+
+1;
---
+++ t/model_MongoDB.t
@@ -0,0 +1,8 @@
+use strict;
+use warnings;
+use Test::More;
+
+
+BEGIN { use_ok 'App::Wollemi::Model::MongoDB' }
+
+done_testing();
